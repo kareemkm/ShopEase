2277e2f9e0bf74cbe374ac623db573f2c5398c88
kareemkm <87376516+kareemkm@users.noreply.github.com>
2024-09-22 20:32:14 +0300

    frist commit

--- lib/Authentication/WelcomeScreen.dart
+++ lib/Authentication/WelcomeScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         body: SingleChildScrollView(
